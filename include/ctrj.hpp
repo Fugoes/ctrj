a571c708c28581ec9c88e2fba9cc52df14ce4339
Fugoes <fugoes.qa@gmail.com>
2020-06-24 14:41:43 +0800

    support validate

--- include/ctrj.hpp
+++ include/ctrj.hpp
@@ -10,13 +10,15 @@
 
 namespace ctrj::ctrj_detail {
 
+/* Schema definition */
 template<const char *K, class V>
 struct field {};
 
-template<class ... KVS>
+template<class ... FIELDS>
 struct object {};
 
-template<class OBJECT>
+/* Value type */
+template<class SCHEMA>
 struct value_type_of;
 
 template<>
@@ -28,30 +30,30 @@ template<>
 struct value_type_of<object<>> {
 };
 
-template<const char *K, class V, class ... KVS>
-struct value_type_of<object<field<K, V>, KVS ...>> :
-    public value_type_of<object<KVS ...>> {
+template<const char *K, class V, class ... FIELDS>
+struct value_type_of<object<field<K, V>, FIELDS ...>> :
+    public value_type_of<object<FIELDS ...>> {
   value_type_of<V> value_{};
 };
 
-template<const char *K, class OBJECT>
+template<const char *K, class SCHEMA>
 struct value_type_of_field;
 
-template<const char *K, class V, class ... KVS>
-struct value_type_of_field<K, object<field<K, V>, KVS ...>> {
-  typedef value_type_of<object<field<K, V>, KVS ...>> type;
+template<const char *K, class V, class ... FIELDS>
+struct value_type_of_field<K, object<field<K, V>, FIELDS ...>> {
+  typedef value_type_of<object<field<K, V>, FIELDS ...>> type;
   typedef value_type_of<V> rtype;
 };
 
-template<const char *K, const char *X, class V, class ... KVS>
-struct value_type_of_field<K, object<field<X, V>, KVS ...>> :
-    public value_type_of_field<K, object<KVS ...>> {
+template<const char *K, const char *X, class V, class ... FIELDS>
+struct value_type_of_field<K, object<field<X, V>, FIELDS ...>> :
+    public value_type_of_field<K, object<FIELDS ...>> {
 };
 
-template<class OBJECT>
+template<class SCHEMA>
 struct value;
 
-template<class OBJECT>
+template<class SCHEMA>
 struct lift_value_type;
 
 template<>
@@ -59,19 +61,113 @@ struct lift_value_type<value_type_of<uint64_t>> {
   typedef value_type_of<uint64_t> type;
 };
 
-template<class ... KVS>
-struct lift_value_type<value_type_of<object<KVS ...>>> {
-  typedef value<object<KVS ...>> type;
+template<class ... FIELDS>
+struct lift_value_type<value_type_of<object<FIELDS ...>>> {
+  typedef value<object<FIELDS ...>> type;
 };
 
-template<class OBJECT>
-struct value : public value_type_of<OBJECT> {
+template<class SCHEMA>
+struct value : public value_type_of<SCHEMA> {
   template<const char *K>
   auto &get() {
-    using field_type = value_type_of_field<K, OBJECT>;
+    using field_type = value_type_of_field<K, SCHEMA>;
     using rtype = typename lift_value_type<typename field_type::rtype>::type;
     auto &r = static_cast<typename field_type::type &>(*this).value_;
     return static_cast<rtype &>(r);
+  }
+};
+
+/* Handler */
+struct handler_base {
+  virtual bool Key(std::string_view str, handler_base **h) = 0;
+  virtual bool Number(std::string_view str, handler_base **h) = 0;
+  virtual bool StartObject(handler_base **h) = 0;
+  virtual bool EndObject(handler_base **h) = 0;
+};
+
+template<class ... FIELDS>
+struct partial_handler;
+
+template<class SCHEMA>
+struct complete_handler;
+
+template<>
+struct partial_handler<> {
+  inline bool Key(std::string_view key, handler_base **h) { return false; }
+  inline bool Number(std::string_view num, handler_base **h) { return false; }
+  inline bool EndObject(handler_base **h) { return true; }
+};
+
+template<>
+struct complete_handler<uint64_t> : public handler_base {
+  handler_base *parent_{nullptr};
+
+  bool Key(std::string_view str, handler_base **h) override { return false; }
+  bool Number(std::string_view str, handler_base **h) override {
+    *h = parent_;
+    return true;
+  }
+  bool StartObject(handler_base **h) override { return false; }
+  bool EndObject(handler_base **h) override { return false; }
+};
+
+template<class ... FIELDS>
+struct complete_handler<object<FIELDS ...>> : public handler_base {
+  handler_base *parent_{nullptr};
+  uint8_t state_{0};
+  partial_handler<FIELDS ...> inner_{};
+
+  bool Key(std::string_view str, handler_base **h) override {
+    return inner_.Key(str, h);
+  }
+  bool Number(std::string_view str, handler_base **h) override {
+    return inner_.Number(str, h);
+  }
+  bool StartObject(handler_base **h) override {
+    if (state_ == 0) {
+      state_ = 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool EndObject(handler_base **h) override {
+    if (state_ == 1) {
+      if (inner_.EndObject(h)) {
+        *h = parent_;
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+};
+
+template<const char *K, class V, class ... FIELDS>
+struct partial_handler<field<K, V>, FIELDS ...> :
+    public partial_handler<FIELDS ...> {
+  uint8_t state_{0};
+  complete_handler<V> sub_handler_{};
+
+  inline bool Key(std::string_view key, handler_base **h) {
+    if (state_ == 0) {
+      if (key == K) {
+        state_ = 1;
+        sub_handler_.parent_ = *h;
+        *h = &sub_handler_;
+        return true;
+      } else {
+        return partial_handler<FIELDS ...>::Key(key, h);
+      }
+    } else {
+      return partial_handler<FIELDS ...>::Key(key, h);
+    }
+  }
+  inline bool Number(std::string_view num, handler_base **h) { return false; }
+  inline bool EndObject(handler_base **h) {
+    return state_ == 1 && partial_handler<FIELDS ...>::EndObject(h);
   }
 };
 
--- main.cpp
+++ main.cpp
@@ -10,18 +10,33 @@ const char _text[] = "text";
 
 using js_obj_schema = x::object<
     x::field<_id, uint64_t>,
-    x::field<
-        _text,
-        x::object<
-            x::field<_id, uint64_t>,
-            x::field<_text, uint64_t>
-        >
-    >
+    x::field<_text, x::object<
+        x::field<_id, uint64_t>,
+        x::field<_text, uint64_t>
+    >>
 >;
 
 int main() {
   x::value<js_obj_schema> js_obj{};
-  js_obj.get<_id>().uint64 = 2;
-  std::cout << js_obj.get<_text>().get<_id>().uint64 << std::endl;
+  x::complete_handler<js_obj_schema> handler{};
+  std::cout << sizeof(handler) << std::endl;
+  x::handler_base *h{&handler};
+
+  bool flag;
+  x::handler_base *t{nullptr};
+
+  if (!h->StartObject(&h)) return 0;
+  if (!h->Key("id", &h)) return 0;
+  if (!h->Number("123", &h)) return 0;
+  if (!h->Key("text", &h)) return 0;
+  if (!h->StartObject(&h)) return 0;
+  if (!h->Key("text", &h)) return 0;
+  if (!h->Number("128", &h)) return 0;
+  if (!h->Key("id", &h)) return 0;
+  if (!h->Number("128", &h)) return 0;
+  if (!h->EndObject(&h)) return 0;
+
+  std::cout << "SUCC" << std::endl;
+
   return 0;
 }
